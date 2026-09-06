b4bc1e4be568a2957d58ac60944b90304da9cb96
sshamimohd-debug <sshamimohd@gmail.com>
2026-09-06 12:01:02 +0530

    CricPro Score Android app + APK build
    * Add Android Gradle settings
    
    * Add Android root build config
    
    * Add Android app module
    
    * Add Android manifest
    
    * Add Android theme
    
    * Add native Android WebView shell
    
    * Add verified Android APK CI build

---
+++ app/src/main/java/in/cricpro/score/MainActivity.java
@@ -0,0 +1,29 @@
+package in.cricpro.score;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
