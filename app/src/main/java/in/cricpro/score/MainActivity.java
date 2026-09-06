80cbada784290f3ebfeef9f7632bbc2ef383e3dd
sshamimohd-debug <sshamimohd@gmail.com>
2026-09-06 12:19:15 +0530

    Fix Android WebView viewport and file chooser

--- app/src/main/java/in/cricpro/score/MainActivity.java
+++ app/src/main/java/in/cricpro/score/MainActivity.java
@@ -1,7 +1,11 @@
 package in.cricpro.score;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -9,6 +13,9 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView webView;
+    private ValueCallback<Uri[]> fileCallback;
+    private static final int FILE_REQUEST = 1001;
+
     @Override public void onCreate(Bundle state) {
         super.onCreate(state);
         webView = new WebView(this);
@@ -19,10 +26,39 @@ public class MainActivity extends Activity {
         s.setDatabaseEnabled(true);
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(false);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setTextZoom(100);
+        webView.setInitialScale(0);
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override public boolean onShowFileChooser(WebView w, ValueCallback<Uri[]> callback, FileChooserParams params) {
+                if (fileCallback != null) fileCallback.onReceiveValue(null);
+                fileCallback = callback;
+                try {
+                    Intent i = params.createIntent();
+                    startActivityForResult(i, FILE_REQUEST);
+                    return true;
+                } catch (Exception e) {
+                    fileCallback = null;
+                    return false;
+                }
+            }
+        });
         webView.loadUrl("file:///android_asset/www/index.html");
     }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_REQUEST && fileCallback != null) {
+            Uri[] result = WebChromeClient.FileChooserParams.parseResult(resultCode, data);
+            fileCallback.onReceiveValue(result);
+            fileCallback = null;
+        }
+    }
+
     @Override public void onBackPressed() {
         if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
     }
